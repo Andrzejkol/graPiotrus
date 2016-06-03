259ab2978283cec9a857f4f00b6b5c1838462362
Andrzejkol <andrzej@olicom.pl>
2016-06-03 07:59:07 +0200

    5

--- www/public_html/app.js
+++ www/public_html/app.js
@@ -1,7 +1,69 @@
-var zestawkart = array(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13);
+var zestawkart = [
+    {symbol: 'A', wybrana: 0},
+    {symbol: 'A', wybrana: 0},
+    {symbol: 'B', wybrana: 0},
+    {symbol: 'B', wybrana: 0},
+    {symbol: 'C', wybrana: 0},
+    {symbol: 'C', wybrana: 0},
+    {symbol: 'D', wybrana: 0},
+    {symbol: 'D', wybrana: 0},
+    {symbol: 'E', wybrana: 0},
+    {symbol: 'E', wybrana: 0},
+    {symbol: 'F', wybrana: 0},
+    {symbol: 'F', wybrana: 0},
+    {symbol: 'G', wybrana: 0},
+    {symbol: 'G', wybrana: 0},
+    {symbol: 'H', wybrana: 0},
+    {symbol: 'H', wybrana: 0},
+    {symbol: 'I', wybrana: 0},
+    {symbol: 'I', wybrana: 0},
+    {symbol: 'J', wybrana: 0},
+    {symbol: 'J', wybrana: 0},
+    {symbol: 'K', wybrana: 0},
+    {symbol: 'K', wybrana: 0},
+    {symbol: 'L', wybrana: 0},
+    {symbol: 'L', wybrana: 0},
+    {symbol: 'P', wybrana: 0}];
+
+var player1 = [];
+var player2 = [];
+var player3 = [];
+var player4 = [];
+
+
+function inicjuj(array) {
+    /* losowa zmiana kolejności liczb */
+    var i = array.length,
+            j = 0,
+            temp;
+
+    while (i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        if (i >= 0 && i <= 5) {
+            player1.push(zestawkart[j]);
+        } else if (i >= 6 && i <= 11) {
+            player2.push(zestawkart[j]);
+        } else if (i >= 12 && i <= 17) {
+            player3.push(zestawkart[j]);
+        } else if (i >= 18 && i <= 25) {
+            player4.push(zestawkart[j]);
+        }
+    }
+    return array;
+}
 
 $(document).ready(function () {
 
+    var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+    var randomNums = inicjuj(nums);
     
+
+    console.log(player1);
+    console.log(player2);
+    console.log(player3);
+    console.log(player4);
 
 });
